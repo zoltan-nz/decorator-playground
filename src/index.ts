ea97ea0e6c556bd2966daf6888f1ffaf673ca4d2
Zoltan <zdebre@gmail.com>
2020-12-21 15:21:00 +1300

    Add simple controller

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,45 @@
-import express, { json } from 'express';
+import express, { json, Request, Response, Router } from 'express';
 import 'reflect-metadata';
 
+class RootController {
+  router: Router = Router();
+
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    this.router.get('/', this.index());
+    this.router.post('/', this.create())
+  }
+
+  index() {
+    return (req: Request, res: Response) => {
+      res.json({
+        message: 'Hello World!'
+      });
+    };
+  }
+
+  create() {
+    return (req: Request, res: Response) => {
+      res.json({
+        message: 'Create World!'
+      });
+    };
+  }
+
+  getRoutes() {
+    return this.router;
+  }
+
+}
+
+
 const app = express();
+const rootController = new RootController();
 app.use(json());
-
-const router = express.Router();
-router.get('/', (req, res) => {
-  res.json({
-    message: 'Hello World!'
-  });
-});
-
-app.use(router);
+app.use(rootController.getRoutes());
 app.listen(3000, () => {
   console.log('Server started on port 3000!');
 });
