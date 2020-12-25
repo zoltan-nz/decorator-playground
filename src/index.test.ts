e309604f0d924ff35e946ec620191edc12b62e0b
Zoltan <zdebre@gmail.com>
2020-12-26 03:48:00 +1300

    Add supertest to test endpoints

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,3 +1,4 @@
+import supertest from "supertest";
 import { App, Logger, RootController, RootService } from './index';
 
 describe('Logger', () => {
@@ -22,3 +23,19 @@ describe('RootService', () => {
     expect(service.findAll()).toEqual('Hello World')
   })
 });
+
+describe('App', () => {
+  test('GET /', async () => {
+    const app = new App();
+    const response = await supertest(app.expressApp).get('/');
+    expect(response.status).toBe(200);
+    expect(response.text).toBe(JSON.stringify({ message: 'Hello World' }));
+  });
+
+  test('POST /', async () => {
+    const app = new App();
+    const response = await supertest(app.expressApp).post('/');
+    expect(response.status).toBe(200);
+    expect(response.text).toBe(JSON.stringify({ message: 'Create World' }));
+  });
+})
