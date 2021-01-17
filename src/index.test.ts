66d5d21c5321761bb3dd7c69e41ad5121da5d8f9
Zoltan <zdebre@gmail.com>
2021-01-17 14:27:00 +1300

    Mock logger and clean up test runner console

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,19 +1,32 @@
 import supertest from 'supertest';
+import { LoggerService } from './services/logger-service';
 import { App, RootService } from './index';
 
+jest.mock('./services/logger-service');
+
 describe('RootService', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
   test('findAll', () => {
     const service = new RootService();
     expect(service.findAll()).toEqual('Hello World');
+    expect(service['logger'].log).toHaveBeenCalledWith('findAll');
   });
 });
 
 describe('App', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
   test('GET /', async () => {
     const app = new App();
     const response = await supertest(app.expressApp).get('/');
     expect(response.status).toBe(200);
     expect(response.text).toBe(JSON.stringify({ message: 'Hello World' }));
+    expect(LoggerService).toHaveBeenCalledTimes(1);
   });
 
   test('POST /', async () => {
@@ -21,6 +34,7 @@ describe('App', () => {
     const response = await supertest(app.expressApp).post('/');
     expect(response.status).toBe(200);
     expect(response.text).toBe(JSON.stringify({ message: 'Create World' }));
+    expect(LoggerService).toHaveBeenCalledTimes(1);
   });
 
   test('GET /demo', async () => {
